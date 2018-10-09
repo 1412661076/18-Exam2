5e1fb06228bdd57c14c3ca4b3f5f118950bd1712
fangr <fangr@rose-hulman.edu>
2018-10-09 18:41:03 -0400

    DONE problem1

--- src/problem1.py
+++ src/problem1.py
@@ -2,8 +2,8 @@
 Exam 2, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Rui Fang.  October 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -48,10 +48,10 @@ def main():
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
 
-    # run_test_problem1a()
-    # run_test_problem1b()
-    # run_test_problem1c()
-    # run_test_problem1d()
+    run_test_problem1a()
+    run_test_problem1b()
+    run_test_problem1c()
+    run_test_problem1d()
 
 
 def run_test_problem1a():
@@ -183,6 +183,7 @@ def run_test_problem1a():
 
 
 def problem1a(strings):
+
     """
     What comes in:
       -- a sequence of strings
@@ -218,9 +219,14 @@ def problem1a(strings):
       :rtype: list of int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    seq = []
+    for k in range(len(strings)):
+        seq = seq + [len(strings[k])]
+    return seq
 
 
 def run_test_problem1b():
@@ -352,6 +358,7 @@ def run_test_problem1b():
 
 
 def problem1b(strings):
+
     """
     What comes in:
       -- a sequence of strings
@@ -391,9 +398,15 @@ def problem1b(strings):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    count = 0
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])):
+            count = count + 1
+    return count
 
 
 def run_test_problem1c():
@@ -542,6 +555,7 @@ def run_test_problem1c():
 # (including possibly your own functions) appropriately, else NO credit for it.
 # -----------------------------------------------------------------------------
 def problem1c(strings):
+
     """
     What comes in:
       -- a sequence of strings
@@ -587,10 +601,14 @@ def problem1c(strings):
       :rtype: bool
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     # See  IMPORTANT  note before the DEF line of this function.
     # -------------------------------------------------------------------------
+
+    if is_prime(problem1b(strings)):
+        return True
+    return False
 
 
 def run_test_problem1d():
@@ -736,6 +754,7 @@ def run_test_problem1d():
 
 
 def problem1d(strings):
+
     """
     What comes in:
       -- a sequence of strings
@@ -774,9 +793,14 @@ def problem1d(strings):
       :rtype: str | -1
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    for k in range(len(strings)):
+        if is_prime(len(strings[k])):
+            return strings[k]
+    return -1
 
 
 ###############################################################################
